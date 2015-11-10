a79ac58ca31bd76b2758b906d745b505d16c826c
greinedt <greinedt@gmail.com>
2015-11-10 16:05:35 -0500

    Add ItemStatus Entity that was missing for some reason

---
+++ GiftList.BAL/Entities/ItemStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using TheGiftList.BAL.BuisinessLogic;
+
+namespace TheGiftList.BAL.Entities
+{
+    public class ItemStatus : Base2
+    {
+        private string _status;
+        public string Status
+        {
+            get { return _status; }
+            set { _status = value; }
+        }
+    }
+}
