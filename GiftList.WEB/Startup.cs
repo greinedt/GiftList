b77ead4ac652b2ca29d313c4f3fb016f27c5e820
greinedt <greinedt@gmail.com>
2016-06-16 21:21:02 -0400

    Changed to Web Forms to finish this faster

---
+++ GiftList.WEB/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GiftList.WEB.Startup))]
+namespace GiftList.WEB
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
